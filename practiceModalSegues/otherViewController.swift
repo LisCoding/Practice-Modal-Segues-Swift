95e801207d3aaad8b7097689985170893678c6ed
Liseth Cardozo <cardozoliseth@gmail.com>
2017-09-11 16:20:09 -0700

    Initial Commit

---
+++ practiceModalSegues/otherViewController.swift
@@ -0,0 +1,32 @@
+//
+//  otherViewController.swift
+//  practiceModalSegues
+//
+//  Created by Liseth Cardozo Sejas on 9/11/17.
+//  Copyright © 2017 Liseth Cardozo Sejas. All rights reserved.
+//
+
+import UIKit
+
+class otherViewController: UIViewController {
+
+    //Go back the previous page
+    
+    @IBAction func dismissButtonWasPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
